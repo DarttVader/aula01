7d0a4c7c56d21ca1db09da9a291403ab45bc5a91
DarttVader <140860974+DarttVader@users.noreply.github.com>
2023-08-28 21:59:46 -0300

    Delete Repositórios/exercícios directory

--- Repositórios/exercícios/Maior número/maior.py
+++
@@ -1,8 +0,0 @@
-valor1 = float(input("digite um valor= "))
-valor2 = float(input("digite outro valor= "))
-
-print (valor1)
-print (valor2)
-resul = valor1 - valor2
-print ("maior valor é= ", valor1 if resul >0 else valor2 )
-
